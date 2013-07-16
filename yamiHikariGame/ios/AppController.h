716e03508911fecd11062d77daf4e14e783247dc
slightair <arksutite@gmail.com>
2013-07-16 23:15:19 +0900

    iOS用プロジェクト作成

---
+++ yamiHikariGame/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  yamiHikariGameAppController.h
+//  yamiHikariGame
+//
+//  Created by slightair on 2013/07/16.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ yamiHikariGame/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  yamiHikariGameAppController.h
+//  yamiHikariGame
+//
+//  Created by slightair on 2013/07/16.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
