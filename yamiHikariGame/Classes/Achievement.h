95b1ed4c88d17f94dc5f6e3a5389752c5bf2d2c7
slightair <arksutite@gmail.com>
2013-08-31 00:30:27 +0900

    iOSのGameCenterの画面を表示する機能を実装した

--- yamiHikariGame/Classes/Achievement.h
+++ yamiHikariGame/Classes/Achievement.h
@@ -18,12 +18,18 @@ class Achievement
 public:
     string _name;
     string _iOSAchievementID;
+    int _process;
+    int _goal;
 
     string getName(){ return _name; }
     string getIOSAchievementID(){ return _iOSAchievementID; }
+    int getProcess(){ return _process; }
+    int getGoal(){ return _goal; }
 
     void setName(string name){ _name = name; }
     void setIOSAchievementID(string id){ _iOSAchievementID = id; }
+    void setProcess(int process){ _process = process; }
+    void setGoal(int goal){ _goal = goal; }
 };
 
 #endif /* defined(__yamiHikariGame__Achievement__) */
