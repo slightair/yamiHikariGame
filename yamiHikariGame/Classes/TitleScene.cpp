fac1e1ffee9f7f7d55692f0be8db89a81f56c0a2
ichiko <ichigo_sky@hotmail.com>
2013-09-14 15:49:14 +0900

    Revert title scene from debug status

--- yamiHikariGame/Classes/TitleScene.cpp
+++ yamiHikariGame/Classes/TitleScene.cpp
@@ -11,7 +11,7 @@
 #include "GameEngine.h"
 
 #define kMainMenuItemPaddingVertical 12
-#define kSubMenuItemPaddingHorizontal 16
+#define kSubMenuItemPaddingHorizontal 72
 #define kSubMenuMarginBottom 32
 #define kResetMenuMarginTop 16
 #define kResetMenuPaddingHorizontal 144
@@ -89,25 +89,10 @@ bool TitleScene::init()
                                                               GameEngine::sharedEngine(),
                                                               menu_selector(GameEngine::showRanking));
 
-        CCMenuItem *showAchievementItem = CCMenuItemLabel::create(CCLabelTTF::create("じっせき", DefaultFontName, FontSizeBig), GameEngine::sharedEngine(), menu_selector(GameEngine::showAchievements));
-
-        CCMenu *subMenu = CCMenu::create(showRankingItem, showAchievementItem, showItemListItem, NULL);
+        CCMenu *subMenu = CCMenu::create(showRankingItem, showItemListItem, NULL);
         subMenu->alignItemsHorizontallyWithPadding(kSubMenuItemPaddingHorizontal);
         subMenu->setPosition(ccp(windowSize.width / 2, showItemListItem->getContentSize().height / 2 + kSubMenuMarginBottom));
         this->addChild(subMenu);
-
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
-
-        CCMenuItem *signInMenuItem = CCMenuItemLabel::create(CCLabelTTF::create("ログイン", DefaultFontName, FontSizeBig),
-                                                            GameEngine::sharedEngine(), menu_selector(GameEngine::signInGoogle));
-        CCMenuItem *signOutMenuItem = CCMenuItemLabel::create(CCLabelTTF::create("ログアウト", DefaultFontName, FontSizeBig),
-                                                              GameEngine::sharedEngine(), menu_selector(GameEngine::signOutGoogle));
-
-        CCMenu *androidMenu = CCMenu::create(signInMenuItem, signOutMenuItem, NULL);
-        androidMenu->setPosition(ccp(windowSize.width / 4, windowSize.height / 2));
-        androidMenu->alignItemsVertically();
-        this->addChild(androidMenu);
-#endif
     }
 
     return result;
