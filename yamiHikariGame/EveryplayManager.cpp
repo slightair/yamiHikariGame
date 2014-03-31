3c47afe4ad1d3d713e6414b431c1c74ca650154f
slightair <arksutite@gmail.com>
2014-03-31 23:47:12 +0900

    Add EveryplayManager

---
+++ yamiHikariGame/EveryplayManager.cpp
@@ -0,0 +1,26 @@
+//
+//  EveryplayManager.cpp
+//  yamiHikariGame
+//
+//  Created by slightair on 2014/03/31.
+//
+//
+
+#include "EveryplayManager.h"g
+
+static EveryplayManager *__sharedManager = NULL;
+
+EveryplayManager *EveryplayManager::sharedManager()
+{
+    if (!__sharedManager) {
+        __sharedManager = new EveryplayManager();
+        __sharedManager->init();
+    }
+
+    return __sharedManager;
+}
+
+bool EveryplayManager::init()
+{
+    return true;
+}
---
+++ yamiHikariGame/EveryplayManager.h
@@ -0,0 +1,28 @@
+//
+//  EveryplayManager.h
+//  yamiHikariGame
+//
+//  Created by slightair on 2014/03/31.
+//
+//
+
+#ifndef __yamiHikariGame__EveryplayManager__
+#define __yamiHikariGame__EveryplayManager__
+
+#include <iostream>
+
+using namespace std;
+
+class EveryplayManager
+{
+protected:
+    bool init();
+    EveryplayManager(){};
+    EveryplayManager(const EveryplayManager& rhs);
+    EveryplayManager& operator=(const EveryplayManager& rhs);
+
+public:
+    static EveryplayManager *sharedManager();
+};
+
+#endif /* defined(__yamiHikariGame__EveryplayManager__) */
